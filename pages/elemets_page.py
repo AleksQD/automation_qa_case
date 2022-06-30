0cfb4ac5af331d892bdf1431cd22e664611f36f3
AleksQD <AleksQD@gmail.com>
2022-06-30 15:22:32 +0300

    added checkbox test

--- pages/elemets_page.py
+++ pages/elemets_page.py
@@ -1,5 +1,7 @@
+
+import random
 from ..generator.generator import generated_person
-from ..locators.elemets_page_locator import TextBoxPageLocators
+from ..locators.elemets_page_locator import CheckBoxPageLocators, TextBoxPageLocators
 from .base_page import BasePage
 
 
@@ -31,3 +33,38 @@ class TextBoxPage(BasePage):
         perm_addr = self.element_is_present(
             self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
         return full_name, email, curr_addr, perm_addr
+
+
+class CheckBoxPage(BasePage):
+    locators = CheckBoxPageLocators()
+
+    def open_full_list(self):
+        self.element_is_visible(self.locators.EXPAND_ALL_BUTTON).click()
+
+    def click_random_checkbox(self):
+        item_list = self.elements_are_visible(self.locators.ITEM_LIST)
+        count = 21
+        while count != 0:
+            item = item_list[random.randint(1, 15)]
+            if count > 0:
+                self.go_to_element(item)
+                item.click()
+                count -= 1
+            else:
+                break
+
+    def get_checked_checkboxes(self):
+        checked_list = self.elements_are_present(self.locators.CHECKED_ITEMS)
+        data = []
+        for box in checked_list:
+            title_item = box.find_element("xpath", self.locators.TITLE_ITEM)
+            data.append(title_item.text)
+        return str(data).replace(' ', '').replace('doc', '').replace('.', '').lower()
+
+    def get_output_result(self):
+        result_list = self.elements_are_present(self.locators.OUTPUT_RESULT)
+        data = []
+        for item in result_list:
+             data.append(item.text)
+        return str(data).replace(' ', '').lower()
+
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,16 +1,28 @@
 
-import time
-from ..pages.elemets_page import TextBoxPage
+import pytest
+from ..pages.elemets_page import CheckBoxPage, TextBoxPage
 
 
 class TestElements:
+    @pytest.mark.skip
     class TestTextBox:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             full_name, email, curr_address, perm_address = text_box_page.fill_all_fields()
             out_name, out_email, out_curr_add, out_perm_add = text_box_page.check_filled_form()
-            assert full_name == out_name
-            assert email == out_email
-            assert curr_address == out_curr_add
-            assert perm_address == out_perm_add
+            assert full_name == out_name, 'the full name does not match'
+            assert email == out_email, 'the email does not match'
+            assert curr_address == out_curr_add, 'the current address does not match'
+            assert perm_address == out_perm_add, 'the permanent address does not match'
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(
+                driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, 'checkboxes have not been selected'
