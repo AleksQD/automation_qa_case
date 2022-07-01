36e3902272dbbd6e3959f4a361dce1942816ac3e
AleksQD <AleksQD@gmail.com>
2022-07-01 21:17:45 +0300

    added  webtable tests

--- generator/generator.py
+++ generator/generator.py
@@ -1,13 +1,19 @@
 from ..data.data import Person
 from faker import Faker
+import random
 
 faker = Faker('ru_Ru')
-#Faker.seed()
+Faker.seed()
 
 
 def generated_person():
     yield Person(
         full_name=faker.first_name() + " " + faker.last_name(),
+        firstname=faker.first_name(),
+        lastname=faker.last_name(),
+        age=random.randint(10, 80),
+        salary=random.randint(10000, 100000),
+        department=faker.job(),
         email=faker.email(),
         current_address=faker.address(),
         permanent_address=faker.address()
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,8 @@
 
+import random
 import time
 import pytest
-from ..pages.elemets_page import CheckBoxPage, RadioButtonPage, TextBoxPage
+from ..pages.elemets_page import CheckBoxPage, RadioButtonPage, TextBoxPage, WebTablePage
 
 
 class TestElements:
@@ -29,6 +30,7 @@ class TestElements:
             output_result = check_box_page.get_output_result()
             assert input_checkbox == output_result, 'checkboxes have not been selected'
 
+    @pytest.mark.skip
     class TestRadioButton:
         def test_radio_button(self, driver):
             radio_button_page = RadioButtonPage(
@@ -37,3 +39,23 @@ class TestElements:
             click_button = radio_button_page.click_random_radio_button()
             output_result = radio_button_page.get_output_result()
             assert click_button == output_result, 'radio-button have not been selected(maybe: random choose the "NO"button  - it is disabled)'
+
+    class TestWebTable:
+        def test_web_table_add_person(self, driver):
+            web_table_page = WebTablePage(
+                driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            new_person = web_table_page.add_new_person()
+            table_result = web_table_page.check_new_person()
+            assert new_person in table_result
+
+        def test_web_table_search_person(self, driver):
+            web_table_page = WebTablePage(
+                driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            keyword = web_table_page.add_new_person()[random.randint(0,5)]
+            web_table_page.search_some_person(keyword)
+            table_result = web_table_page.check_search_person()
+            print(table_result)
+            print(keyword)
+            assert keyword in table_result, "The person was not found in table"
