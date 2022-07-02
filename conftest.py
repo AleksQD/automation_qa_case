496491952c3d6b47e9f3e9d14a62c05b364f64c4
AleksQD <AleksQD@gmail.com>
2022-07-03 00:52:18 +0300

    add page load strategy

--- conftest.py
+++ conftest.py
@@ -3,11 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+# The page load strategy:
+#  normal-Used by default, 
+#  eager - DOM access is ready, but other resources like images may still be loading
+options = webdriver.ChromeOptions()
+options.page_load_strategy = 'eager'
+
 
 @pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(options=options,
+                              service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
     driver.quit()
